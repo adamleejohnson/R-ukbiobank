cd02e27a8f4085939d442238dc6a6d875da7666a
adam <adamleejohnson@gmail.com>
2021-10-02 00:28:08 -0400

    ICD10 lookup functions

---
+++ R/ICD10_lookup.R
@@ -0,0 +1,139 @@
+#' ICD10 Lookup
+#' @param data data table
+#' @param .icd10_field example ICD10 field
+#' @param ... ICD10 codes to look up
+#' @export
+ICD10_lookup <- function(data, ..., .icd10_field = f.41270.0.0.Diagnoses_ICD10) {
+
+  icd10_columns <- expand_array(data, {{ .icd10_field }})
+  icd10_lookups <-
+    tibble(icd10 = c(...)) %>%
+    left_join(coding_icd10, by = "icd10") %>%
+    pull(coding)
+
+  data %>%
+    select(!!!icd10_columns) %>%
+    mutate(across(everything(), ~.x %in% icd10_lookups)) %>%
+    rowwise() %>% mutate(any(c_across(everything()))) %>%
+    pull()
+}
+
+#' @noRd
+icd10_text_to_codings <- function(text) {
+  codes <-
+    text %>%
+    str_trim() %>% str_split("\n", simplify = T) %>%
+    str_trim() %>% str_split("\\s", 2, simplify = T)
+  codes[,1]
+}
+
+#' @rdname ICD10_lookup
+ICD10_heart_failure <- function(data, .icd10_field = f.41270.0.0.Diagnoses_ICD10) {
+  codings <- icd10_text_to_codings("
+    I50 Heart failure
+    I50.0 Congestive heart failure
+    I50.1 Left ventricular failure
+    I50.9 Heart failure, unspecified
+  ")
+  ICD10_lookup(data, codings, .icd10_field = {{.icd10_field}})
+}
+
+#' @rdname ICD10_lookup
+ICD10_pulm_htn <- function(data, .icd10_field = f.41270.0.0.Diagnoses_ICD10) {
+  codings <- icd10_text_to_codings("
+    I27.0 Primary pulmonary hypertension
+    I27.2 Other secondary pulmonary hypertension
+    I27.8 Other specified pulmonary heart diseases
+    I27.9 Pulmonary heart disease, unspecified
+  ")
+  ICD10_lookup(data, codings, .icd10_field = {{.icd10_field}})
+}
+
+#' @rdname ICD10_lookup
+ICD10_pulm_embolism <- function(data, .icd10_field = f.41270.0.0.Diagnoses_ICD10) {
+  codings <- icd10_text_to_codings("
+    I26 Pulmonary embolism
+    I26.0 Pulmonary embolism with mention of acute cor pulmonale
+    I26.9 Pulmonary embolism without mention of acute cor pulmonale
+  ")
+  ICD10_lookup(data, codings, .icd10_field = {{.icd10_field}})
+}
+
+#' @rdname ICD10_lookup
+ICD10_pulm_misc <- function(data, .icd10_field = f.41270.0.0.Diagnoses_ICD10) {
+  codings <- icd10_text_to_codings("
+    I28 Other diseases of pulmonary vessels
+    I28.0 Arteriovenous fistula of pulmonary vessels
+    I28.1 Aneurysm of pulmonary artery
+    I28.8 Other specified diseases of pulmonary vessels
+    I28.9 Disease of pulmonary vessels, unspecified
+    I37 Pulmonary valve disorders
+    I37.0 Pulmonary valve stenosis
+    I37.1 Pulmonary valve insufficiency
+    I37.2 Pulmonary valve stenosis with insufficiency
+    I37.8 Other pulmonary valve disorders
+    I37.9 Pulmonary valve disorder, unspecified
+    I39.3 Pulmonary valve disorders in diseases classified elsewhere
+    Q21.4 Aortopulmonary septal defect
+    Q22 Congenital malformations of pulmonary and tricuspid valves
+    Q22.0 Pulmonary valve atresia
+    Q22.1 Congenital pulmonary valve stenosis
+    Q22.2 Congenital pulmonary valve insufficiency
+    Q22.3 Other congenital malformations of pulmonary valve
+    Q24.3 Pulmonary infundibular stenosis
+    Q25.5 Atresia of pulmonary artery
+    Q25.6 Stenosis of pulmonary artery
+    Q25.7 Other congenital malformations of pulmonary artery
+    Q26.2 Total anomalous pulmonary venous connexion
+    Q26.3 Partial anomalous pulmonary venous connexion
+    Q26.4 Anomalous pulmonary venous connexion, unspecified
+    S25.4 Injury of pulmonary blood vessels
+  ")
+  ICD10_lookup(data, codings, .icd10_field = {{.icd10_field}})
+}
+
+#' @rdname ICD10_lookup
+ICD10_pulm_ILD <- function(data, .icd10_field = f.41270.0.0.Diagnoses_ICD10) {
+  codings <- icd10_text_to_codings("
+    J70.2 Acute drug-induced interstitial lung disorders
+    J70.3 Chronic drug-induced interstitial lung disorders
+    J70.4 Drug-induced interstitial lung disorder, unspecified
+    J84 Other interstitial pulmonary diseases
+    J84.1 Other interstitial pulmonary diseases with fibrosis
+    J84.8 Other specified interstitial pulmonary diseases
+    J84.9 Interstitial pulmonary disease, unspecified
+    E84.0 Cystic fibrosis with pulmonary manifestations
+  ")
+  ICD10_lookup(data, codings, .icd10_field = {{.icd10_field}})
+}
+
+#' @rdname ICD10_lookup
+ICD10_pulm_emphysema <- function(data, .icd10_field = f.41270.0.0.Diagnoses_ICD10) {
+  codings <- icd10_text_to_codings("
+    J43 Emphysema
+    J43.1 Panlobular emphysema
+    J43.2 Centrilobular emphysema
+    J43.8 Other emphysema
+    J43.9 Emphysema, unspecified
+    J44 Other chronic obstructive pulmonary disease
+    J44.0 Chronic obstructive pulmonary disease with acute lower respiratory infection
+    J44.1 Chronic obstructive pulmonary disease with acute exacerbation, unspecified
+    J44.8 Other specified chronic obstructive pulmonary disease
+    J44.9 Chronic obstructive pulmonary disease, unspecified
+    J98.2 Interstitial emphysema
+    J98.3 Compensatory emphysema
+  ")
+  ICD10_lookup(data, codings, .icd10_field = {{.icd10_field}})
+}
+
+#' @rdname ICD10_lookup
+ICD10_heart_pulm_transplant <- function(data, .icd10_field = f.41270.0.0.Diagnoses_ICD10) {
+  codings <- icd10_text_to_codings("
+    T86.2 Heart transplant failure and rejection
+    T86.3 Heart-lung transplant failure and rejection
+    Z94.2 Lung transplant status
+    Z94.3 Heart and lungs transplant status
+    Z99.4 Dependence on artificial heart
+  ")
+  ICD10_lookup(data, codings, .icd10_field = {{.icd10_field}})
+}
--- R/field_helper.R
+++ R/field_helper.R
@@ -2,11 +2,34 @@
 expand_instances <- function(field_name, indices = 0:3) {
   field_name <- enquo(field_name)
   field_name <- as.character(quo_get_expr(field_name))
-  re <- "(f\\.\\d+\\.)\\d(\\.\\.*)"
-  if (str_detect(field_name, re)) {
-    result <- str_replace(field_name, re, paste0("\\1", indices, "\\2"))
-  } else {
-    result <- field_name
-  }
+  re <- "(.*\\d+\\.)\\d(\\.\\d\\..*)"
+  stopifnot("Column name does not contain Field.Instance.Array" = str_detect(field_name, re))
+  result <- str_replace(field_name, re, paste0("\\1", indices, "\\2"))
   sapply(result, as.symbol, USE.NAMES = F)
+}
+
+#' @noRd
+expand_array <- function(data, field_name) {
+  field_name <- enquo(field_name)
+  field_name <- as.character(quo_get_expr(field_name))
+  re <- "(.*[^\\d])?(\\d+)(\\.)(\\d)(\\.)(\\d)([^\\d].*)?"
+  stopifnot("Column name does not contain Field.Instance.Array" = str_detect(field_name, re))
+
+  # Build regex expression to test against column names
+  field_matcher <- str_match(field_name, re)[1,-1]
+  field_matcher <- paste0(c(quotemeta(field_matcher[1:5]), "(\\d+)", quotemeta(field_matcher[7])), collapse = "")
+  matched_fields <- colnames(data)[str_detect(colnames(data), field_matcher)]
+
+  # extract array numbers from all the matched fields and check that all the array indices are present
+  indices <- as.integer(str_match(matched_fields, field_matcher)[,-1])
+  stopifnot("Array indices are missing" = setequal(indices, min(indices):max(indices)))
+
+  # return result
+  sapply(matched_fields, as.symbol, USE.NAMES = F)
+}
+
+#' Helper to escape characters when building regex strings
+#' @noRd
+quotemeta <- function(string) {
+  str_replace_all(string, "(\\W)", "\\\\\\1")
 }
