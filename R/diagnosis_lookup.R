040b3a9ba80de4e982c57962a610db80811f8c7b
adam <adamleejohnson@gmail.com>
2021-10-03 02:24:27 -0400

    Diagnosis lookup

---
+++ R/diagnosis_lookup.R
@@ -0,0 +1,41 @@
+#' Diagnosis (self-reported) Lookup
+#' @param data data table
+#' @param .icd10_field example diagnosis field
+#' @param ... Diagnosis codes to look up
+#' @export
+diagnosis_lookup <- function(data, ..., .diagnosis_field = f.20002.0.0.Non_cancer_illness_code_self_reported) {
+
+  dx_columns <- expand_instance_and_array(data, {{ .diagnosis_field }})
+  dx_codes <- c(...)
+
+  data %>%
+    select(!!!dx_columns) %>%
+    mutate(across(everything(), ~.x %in% dx_codes)) %>%
+    rowwise() %>% mutate(any(c_across(everything()))) %>%
+    pull()
+}
+
+#' @noRd
+dx_lines_to_codes <- function(text) {
+  meaning <-
+    text %>%
+    str_trim() %>%
+    str_split("\n", simplify = T) %>%
+    str_trim()
+  coding_medical_conditions %>%
+    right_join(tibble(meaning), by = "meaning") %>%
+    pull(coding)
+}
+
+diagnosed_chf <- function(data,
+                          .diagnosis_field = f.20002.0.0.Non_cancer_illness_code_self_reported,
+                          .icd10_field = f.41270.0.0.Diagnoses_ICD10) {
+
+  codings <- dx_lines_to_codes("
+    heart failure/pulmonary odema
+  ")
+
+  dx <- diagnosis_lookup(data, codings, .diagnosis_field = {{.diagnosis_field}})
+  icd10 <- ICD10_heart_failure(data, .icd10_field = {{.icd10_field}})
+  return(dx | icd10)
+}
